e6d98ba8185c16688e4eb1f03012781e37536708
Macc Liu <maccliu@qq.com>
2017-11-16 11:31:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# htmleditor
+
+`htmleditor` 是一个小巧的，基于jquery的html页面编辑器。
---
+++ src/jquery.htmleditor.js
@@ -0,0 +1,7 @@
+/**
+ * htmleditor  -- A Tiny HTML Editor based on jquery.
+ * Copyright (c) Zeupin LLC. (http://zeupin.com)
+ *
+ * Licensed under The MIT License.
+ * Redistributions of files MUST retain the above copyright notice.
+ */
