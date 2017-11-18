f892dd646159df66980824d31ad689e70101388d
Macc Liu <maccliu@qq.com>
2017-11-19 00:24:38 +0800

    dev

--- src/jquery.htmleditor.js
+++ src/jquery.htmleditor.js
@@ -5,3 +5,19 @@
  * Licensed under The MIT License.
  * Redistributions of files MUST retain the above copyright notice.
  */
+
+(function ($) {
+  $.fn.htmleditor = function(options) {
+    // 默认设置
+    var defaults = {
+    };
+
+    // 最终设置
+    var settings = $.extend(defaults, options);
+
+    // 对选择集的每个元素依次处理
+    return this.each(function(idx, ele){
+      console.log(ele.outerHTML);
+    });
+  };
+})(jQuery);
